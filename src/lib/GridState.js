3fb30f569988af07bc3d73cbeda435efde9f71b0
alexMA <almiddeleer@gmail.com>
2020-01-28 22:01:25 -0500

    refactor

--- src/lib/GridState.js
+++ src/lib/GridState.js
@@ -3,11 +3,38 @@ import { PORT } from "./GridCell.js";
 import { SHIP } from "./GridCell.js";
 const rowLen = 20;
 const columnLen = 20;
-const spot = (x, y) => `${x},${y}`;
+
+class Port {
+  constructor(x, y) {
+    this.coords = new CartesianCoords(x, y);
+  }
+}
+
+class Ship {
+  constructor(x, y) {
+    this.coords = new CartesianCoords(x, y);
+  }
+}
+
+class CartesianCoords {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  equals(x, y) {
+    return this.x === x && this.y === y;
+  }
+}
+
 export default class GridState {
   constructor() {
-    const ships = [spot(2, 2), spot(10, 18), spot(16, 4)];
-    const ports = [spot(5, 11), spot(12, 17), spot(3, 6), spot(13, 5)];
+    const ships = [new Ship(2, 2), new Ship(10, 18), new Ship(16, 4)];
+    const ports = [
+      new Port(5, 11),
+      new Port(12, 17),
+      new Port(3, 6),
+      new Port(13, 5)
+    ];
     const rows = [];
     this.rows = rows;
     this.ships = ships;
@@ -16,14 +43,14 @@ export default class GridState {
   }
 
   rebuildGrid() {
+    this.rows = [];
     const { rows, ships, ports } = this;
     for (let i = 0; i < rowLen; i++) {
       rows.push([]);
       for (let j = 0; j < columnLen; j++) {
-        const aSpot = spot(i, j);
-        if (ports.indexOf(aSpot) > -1) {
+        if (ports.find(p => p.coords.equals(i, j))) {
           rows[i].push(new GridCell(PORT));
-        } else if (ships.indexOf(aSpot) > -1) {
+        } else if (ships.find(p => p.coords.equals(i, j))) {
           rows[i].push(new GridCell(SHIP));
         } else {
           rows[i].push(new GridCell());
