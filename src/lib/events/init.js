9c34adf92a1fd04f38ae97d83dda07b75bfe8d70
alexMA <almiddeleer@gmail.com>
2020-02-18 21:48:20 -0500

    Revert "refactor"
    This reverts commit 1526abd7132cc59d501e8da1d90d48916ed09549.

--- src/lib/events/init.js
+++
@@ -1,8 +0,0 @@
-import { INIT_EVENT } from "../event-bus.js";
-import AppEvent from "./app.js";
-export class InitEvent extends AppEvent {
-  constructor(app) {
-    super(INIT_EVENT, "app initialized");
-    this.app = app;
-  }
-}
